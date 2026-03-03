4c33540452b3376a148e0f18e79be1063702a1ae
abir781 <hasan.abir3176@gmail.com>
2026-03-03 13:04:56 +0600

    kkkdf

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = 5000
 const mongoose = require('mongoose');
 const Habit = require('./models/Habit');
 
 
 app.use(express.json());
+app.use(cors());
 
 
-const habitsData = [
-  {
-    name: "Exercise",
-    description: "ghgh",
-    frequency: "Daily",
-    status: "pending"
-  },
-  {
-    name: "Read Book",
-    description: "ghgh",
-    frequency: "Daily",
-    status: "pending"
-  },
-  {
-    name: "Meditation",
-    description: "ghgh",
-    frequency: "Daily",
-    status: "pending"
-  },
-  {
-    name: "Drink Water",
-    description: "ghgh",
-    frequency: "Daily",
-    status: "pending"
-  }
-];
+// const habitsData = [
+//   {
+//     name: "Exercise",
+//     description: "ghgh",
+//     frequency: "Daily",
+//     status: "pending"
+//   },
+//   {
+//     name: "Read Book",
+//     description: "ghgh",
+//     frequency: "Daily",
+//     status: "pending"
+//   },
+//   {
+//     name: "Meditation",
+//     description: "ghgh",
+//     frequency: "Daily",
+//     status: "pending"
+//   },
+//   {
+//     name: "Drink Water",
+//     description: "ghgh",
+//     frequency: "Daily",
+//     status: "pending"
+//   }
+// ];
 
 
 
@@ -59,18 +61,23 @@ async function run() {
     await mongoose.connect(uri, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
 
-   app.get('/habits',async(req,res)=>{
-      const habits = await Habit.find();
-      res.json(habitsData);
-   })
+  //  app.get('/habits',async(req,res)=>{
+  //     const habits = await Habit.find();
+  //     res.json(habitsData);
+  //  })
 
-   app.post('/habitscollection', async (req, res) => {
-  try {
-    const savedHabits = await Habit.insertMany(req.body); // ✅ array support
-    res.status(201).json(savedHabits);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
+//    app.post('/habitscollection', async (req, res) => {
+//   try {
+//     const savedHabits = await Habit.insertMany(req.body); // ✅ array support
+//     res.status(201).json(savedHabits);
+//   } catch (err) {
+//     res.status(400).json({ error: err.message });
+//   }
+// });
+
+app.post('/habitscollection', async (req, res) => {
+  const savedHabit = await Habit.create(req.body); // single object save
+  res.status(201).json(savedHabit);
 });
 
     console.log("Pinged your deployment. You successfully connected to MongoDB with the help of mongoose!");
--- models/Habit.js
+++ models/Habit.js
@@ -1,10 +1,23 @@
+// const mongoose = require('mongoose');
+
+// const habitSchema = new mongoose.Schema({
+//   habitname: { type: String, required: true },
+//   category: String,
+//   frequency: String,
+  
+// });
+
+// const Habit = mongoose.model('Habit', habitSchema);
+
+// module.exports = Habit;
+
+
 const mongoose = require('mongoose');
 
 const habitSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: String,
-  frequency: String,
-  status: { type: String, default: "pending" }
+  habitname: { type: String, required: true },
+  category: { type: String, required: true },
+  frequency: { type: String, required: true },
 });
 
 const Habit = mongoose.model('Habit', habitSchema);
